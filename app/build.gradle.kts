cabddfa318af5b6042b76ea5081df0cb1a2b4b4d
Sergey Troshin <wr.main@gmail.com>
2026-03-06 20:53:35 +0100

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.fridgelist.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.fridgelist.auth"
 
